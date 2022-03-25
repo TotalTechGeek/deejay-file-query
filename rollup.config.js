3dd8bc44c06a9138362e028b290cbfe5c777c876
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2022-03-24 19:53:22 -0500

    Improve build for extensions

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser'
+import json from '@rollup/plugin-json'
 
 function extension (file) {
     const replacements = file ? {
@@ -32,11 +33,21 @@ export default {
         format: 'cjs',
         file: 'dist/bundle.js',
     },
+    
     plugins: [
         extension(process.env.DEEJAY_EXTENSION),
         hash(),
-        commonjs(),
         nodeResolve(),
-        terser()
+        commonjs(),
+        replace({
+            // This is a very explicit fix to deal with a rollup issue, but it's corrected with this.
+            // These fixes should hopefully be rare.
+            'const hexoid = require$$2;': 'const hexoid = require$$2["default"];',
+            "import RDKafka from './inputs/rdkafka.js'": '',
+            "RDKafka,": '',
+            delimiters: ['', '']
+        }),
+        terser(),
+        json()
     ],
 };
